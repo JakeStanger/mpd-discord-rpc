fc2d840d1c1f10cfb2ee8289a309d33240080d28
OliverKovacs <oliver.kovacs.dev@gmail.com>
2022-02-21 14:56:28 +0100

    Added new config options

--- src/defaults.rs
+++ src/defaults.rs
@@ -7,3 +7,7 @@ pub const DEFAULT_HOST: &str = "localhost:6600";
 pub const DETAILS_FORMAT: &str = "$title";
 pub const STATE_FORMAT: &str = "$artist / $album";
 pub const TIMESTAMP_MODE: &str = "elapsed";
+pub const LARGE_IMAGE: &str = "notes";
+pub const SMALL_IMAGE: &str = "notes";
+pub const LARGE_TEXT: &str = "";
+pub const SMALL_TEXT: &str = "";
